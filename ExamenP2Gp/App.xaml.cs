cbb0609543bd3635972ba88d8c24f9dd033c37f8
GiovannyPadilla <146996637+GiovannyPadilla@users.noreply.github.com>
2025-06-02 07:13:03 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP2Gp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenP2Gp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenP2Gp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenP2Gp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
